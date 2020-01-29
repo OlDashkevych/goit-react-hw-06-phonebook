1c949568ff15235bd5b2032f7539971da4fe3177
Йошкарали <dashkevich672@gmail.com>
2020-01-29 13:43:51 +0200

    Updates

---
+++ precache-manifest.544a30d13dd5328210fdf96fd8d3ff04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd015fa7622772554e45a0ddb33279bc",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "dacc09d8a4c32ae9618c",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.e6d30a7e.chunk.css"
+  },
+  {
+    "revision": "9f42c3fefaa312dca00a",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.12b540e9.chunk.js"
+  },
+  {
+    "revision": "dacc09d8a4c32ae9618c",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.0b0d20ce.chunk.js"
+  },
+  {
+    "revision": "3288f23a783c8f75d9e7",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.e7453df4.js"
+  },
+  {
+    "revision": "efc182efb7163e84342f50f4b395c976",
+    "url": "/goit-react-hw-06-phonebook/static/media/alert.efc182ef.png"
+  }
+]);
